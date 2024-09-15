2234d67a9af6f476cb591d121465760b0d83c245
shahank42 <shounakghosh42@gmail.com>
2024-09-16 04:01:14 +0530

    feed page and new post page scaffolded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,42 @@
 <script>
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.css';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as ToggleGroup from '$lib/components/ui/toggle-group';
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import { cn } from '$lib/utils';
+	import { buttonVariants } from '$lib/components/ui/button';
+
+	const queryClient = new QueryClient();
+
+	const POST_TYPES = ['rant', 'confession', 'opinion'];
 </script>
 
-<main class="flex flex-col gap-3 px-4">
-	<slot></slot>
-</main>
+<QueryClientProvider client={queryClient}>
+	<header class="sticky top-0 flex w-full flex-col border-b border-input bg-background py-3">
+		<div class="px-4">
+			<a
+				class={cn(buttonVariants({ variant: 'secondary', size: 'lg' }), 'w-full')}
+				href="/new-post"
+			>
+				New Post
+			</a>
+		</div>
+
+		<div class="pt-4">
+			<ScrollArea class="flex w-full flex-col items-start py-1" orientation="horizontal">
+				<ToggleGroup.Root type="multiple">
+					{#each POST_TYPES as postType}
+						<ToggleGroup.Item value={postType} class="text-muted-foreground"
+							>#{postType}</ToggleGroup.Item
+						>
+					{/each}
+				</ToggleGroup.Root>
+			</ScrollArea>
+		</div>
+	</header>
+
+	<main class="flex flex-col gap-3 px-4 py-4">
+		<slot></slot>
+	</main>
+</QueryClientProvider>
--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+	import { Button } from '$lib/components/ui/button';
 	import FeedCard from './feed-card.svelte';
 </script>
 
-<FeedCard />
+<div class="flex flex-col gap-4">
+	<FeedCard />
+	<FeedCard />
+	<FeedCard />
+</div>
